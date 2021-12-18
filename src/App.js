400432bc262005b4e45349194a94bdca67dfa217
Theodosis <karataristheodosis@gmail.com>
2021-12-18 14:46:22 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Home from "./components/Home/Home";
+
+const App = () => {
+  console.log(Home);
+  return <Home></Home>;
+};
+
+export default App;
---
+++ src/components/Home/styles.js
@@ -0,0 +1,4 @@
+export const styles = {
+  homeAlign: { height: "100vh" },
+  fullWidth: { width: "100%", textAlign: "center" },
+};
