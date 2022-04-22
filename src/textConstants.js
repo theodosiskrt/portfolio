d6e32e7db87adb438a0b56875e6da4a515f8768b
theodosis <karataristheodosis@gmail.com>
2022-04-22 11:27:26 +0300

    fixes/additions in the text constants

--- src/textConstants.js
+++ src/textConstants.js
@@ -15,10 +15,12 @@ export const getAboutText = () => (
 export const backgroundTitle = "Background";
 export const skillsTitle = "Skills";
 export const backgroundText =
-  "I am a self-taught software developer from Greece. I started my journey in 2020 learning the basics of HTML, CSS and Javascript as well as some back-end logic. I decided to move on to front-end development in the summer of 2021 when I started studying a very reputable front-end library called React.js. That's how I got my very first front-end development position and this helped me improve as a developer. I have gained a lot of new knowledge, not just in Javascript but in many other tools, such as Git and even Figma!";
+  "I am a self-taught software developer from Greece. I started my journey in 2020 learning the basics of HTML, CSS and Javascript as well as some Back End logic. I decided to move on to Front End Developement in the summer of 2021 when I started studying a very reputable Front End library called React.js. That's how I got my very first front-end development position and this helped me improve as a developer. I have gained a lot of new knowledge, not just in Javascript but in many other tools, such as Git, Figma and many more.";
 export const skills = [
   "Javascript",
+  "Typescript",
   "React.js",
+  "Redux",
   "React Testing",
   "Material UI",
   "Plotly",
