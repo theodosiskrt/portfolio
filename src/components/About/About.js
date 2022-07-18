190f9825f22ed8f234866e782ec5db1991d89faf
Theodosis <karataristheodosis@gmail.com>
2022-07-18 20:04:34 +0300

    Make about me page responsive down to 300px

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,28 +5,21 @@ import { styles } from "./styles";
 
 const useStyles = makeStyles(styles);
 
-const About = ({ header, text }) => {
+const About = ({ header }) => {
   const classes = useStyles();
 
   return (
-    <Grid className={classes.container} alignItems="center" container>
-      <Grid
-        item
-        container
-        justifyContent="center"
-        xs={12}
-        className={classes.innerContainer}
-      >
-        <Grid item xs={7}>
-          <Typography
-            className={`${classes.fullWidth} ${classes.underline}`}
-            variant="h1"
-          >
-            {header}
-          </Typography>
-        </Grid>
+    <Grid className={classes.container} container>
+      <Grid item container justifyContent="center" xs={12}>
+        <Typography
+          sx={{ ...styles.fullWidth, ...styles.header }}
+          className={`${classes.fullWidth} ${classes.header}`}
+          variant="h1"
+        >
+          {header}
+        </Typography>
 
-        <Grid items xs={6}>
+        <Grid items xs={12} sm={10} md={8} lg={6}>
           <Typography className={classes.fullWidth} variant="h6">
             Hi! I am a 19-year-old passionate software developer from Greece who
             is interested in <br />
--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -1,36 +1,20 @@
 import BgImage from "../../images/About2.jpg";
 
-export const styles = () => ({
+export const styles = {
   container: {
     scrollSnapAlign: "start",
-    height: "100vh",
     background: `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url(${BgImage})`,
-    // backgroundImage: `url(${BgImage})`,
     backgroundSize: "cover",
-    backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
-    backgroundColor: "red",
 
-    maxWidth: `100%`,
+    backgroundPosition: "center",
+    height: "100vh",
     color: "#fff",
   },
-  underline: { borderBottom: "3px solid white", borderWidth: "3px" },
+  header: {
+    fontSize: { xs: "3.75em", sm: "5em" },
+    alignSelf: { xs: "start", md: "end" },
+    marginTop: { xs: "1em", md: "" },
+  },
   fullWidth: { width: "100%", textAlign: "center" },
-});
-
-// .shinyText {
-//   transform: translate(-50%, -50%);
-//   padding: 12px 150px;
-//   color: #ffffff;
-//   font-size: 1.5em;
-//   background: linear-gradient(to right, #727272 0, white 10%, #727272 20%);
-//   background-position: 0;
-//   -webkit-background-clip: text;
-//   -webkit-text-fill-color: transparent;
-//   animation: shine 7s infinite linear;
-//   animation-fill-mode: forwards;
-//   -webkit-text-size-adjust: none;
-
-//   text-decoration: none;
-//   white-space: nowrap;
-// }
+};
