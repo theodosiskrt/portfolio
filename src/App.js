2609fa6b5256776f575609bc60ba8e6c872be570
Theodosis <karataristheodosis@gmail.com>
2022-07-18 20:04:34 +0300

    Add download cv button to about me page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { makeStyles } from "@mui/styles";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import { styles } from "./styles";
-import { name, role, aboutTitle, aboutText } from "./textConstants";
+import { name, role, aboutTitle } from "./textConstants";
 
 const useStyles = makeStyles(styles);
 
 const App = () => {
   const classes = useStyles();
   return (
-    <Grid className={classes.container}>
-      <Home header={name} subheader={role} pageIndex={0}></Home>
-      <About header={aboutTitle} text={aboutText} pageIndex={1}></About>
+    <Grid container className={classes.container}>
+      <Home header={name} subheader={role} />
+      <About header={aboutTitle} />
     </Grid>
   );
 };
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,35 +1,45 @@
 import { Grid, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 
+import DownloadCV from "./DownloadCV";
 import { styles } from "./styles";
 
-const useStyles = makeStyles(styles);
-
 const About = ({ header }) => {
-  const classes = useStyles();
-
   return (
-    <Grid className={classes.container} container>
-      <Grid item container justifyContent="center" xs={12}>
-        <Typography
-          sx={{ ...styles.fullWidth, ...styles.header }}
-          className={`${classes.fullWidth} ${classes.header}`}
-          variant="h1"
-        >
+    <Grid item container sx={styles.container} justifyContent="center" xs={12}>
+      <Grid container item xs={12} sx={styles.height33percent}>
+        <Typography sx={{ ...styles.fullWidth, ...styles.header }} variant="h1">
           {header}
         </Typography>
-
-        <Grid items xs={12} sm={10} md={8} lg={6}>
-          <Typography className={classes.fullWidth} variant="h6">
-            Hi! I am a 19-year-old passionate software developer from Greece who
-            is interested in <br />
-            <span className="shinyText">Front End Development</span>
-            <br /> as well as <br />
-            <span className="shinyText">Mobile and Desktop Design.</span>
-            <br /> I am constantly looking for the next brain-stimulating
-            project. Always excited to work with new frameworks and tools.
-          </Typography>
-        </Grid>
+      </Grid>
+      <Grid
+        container
+        item
+        xs={12}
+        sm={10}
+        md={8}
+        lg={6}
+        alignItems="center"
+        sx={styles.height33percent}
+      >
+        <Typography sx={styles.fullWidth} variant="h6">
+          Hi! I am a 19-year-old passionate software developer from Greece who
+          is interested in <br />
+          <span className="shinyText">Front End Development</span>
+          <br /> as well as <br />
+          <span className="shinyText">Mobile and Desktop Design.</span>
+          <br /> I am constantly looking for the next brain-stimulating project.
+          Always excited to work with new frameworks and tools.
+        </Typography>
+      </Grid>
+      <Grid
+        container
+        item
+        xs={12}
+        sx={styles.height33percent}
+        justifyContent="center"
+        alignItems="start"
+      >
+        <DownloadCV styles={styles.button} />
       </Grid>
     </Grid>
   );
